54b384202111701a16fee450d8e94609d93faace
Fco Javier del Castillo <javicastillo92@gmail.com>
2019-05-22 17:01:31 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#Autores: Fco Javier del Castillo y Carlos Suárez
+#Autor: Fco Javier del Castillo Ramírez
 
 import telebot
 from telebot import types
